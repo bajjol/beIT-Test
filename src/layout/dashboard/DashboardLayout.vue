4c96eb15e8e78428270789eb7da7464b2ac36fb7
bajjol <dadakansajah@gmail.com>
2022-10-21 16:09:26 +0700

    21-10-2022

---
+++ src/layout/dashboard/DashboardLayout.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="wrapper">
+    <div class="main-panel">
+      <top-navbar></top-navbar>
+
+      <dashboard-content>
+
+      </dashboard-content>
+
+      <content-footer></content-footer>
+    </div>
+  </div>
+</template>
+<style lang="scss">
+</style>
+<script>
+import TopNavbar from "./TopNavbar.vue";
+import ContentFooter from "./ContentFooter.vue";
+import DashboardContent from "./Content.vue";
+import MobileMenu from "./MobileMenu";
+export default {
+  components: {
+    TopNavbar,
+    ContentFooter,
+    DashboardContent,
+    MobileMenu
+  },
+  methods: {
+    toggleSidebar() {
+      if (this.$sidebar.showSidebar) {
+        this.$sidebar.displaySidebar(false);
+      }
+    }
+  }
+};
+</script>
