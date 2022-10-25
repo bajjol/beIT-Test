fdc0128ecf87242924d71213b53515eaf3b2c360
bajjol <dadakansajah@gmail.com>
2022-10-25 16:40:36 +0700

    api

--- src/pages/Dashboard.vue
+++ src/pages/Dashboard.vue
@@ -142,89 +142,97 @@
       UserTable
     },
     data() {
+      fetch('http://ecocim-backend-brisatelitv2.beit.co.id/api/Dashboard/GetDashboardWidgetData?widget=PSCF_UPS_RoomTH&chart_name=PSCF_UPS_RoomTH')
+          .then(function(response) {
+            return response.json()
+          })
+          .then(function(data) {
+              const data2 = JSON.stringify(data)
+              console.log(data2);
+          })
       return {
-        bigLineChart: {
-          allData: [
-            [100, 70, 90, 70, 85, 60, 75, 60, 90, 80, 110, 100],
-            [80, 120, 105, 110, 95, 105, 90, 100, 80, 95, 70, 120],
-            [60, 80, 65, 130, 80, 105, 90, 130, 70, 115, 60, 130]
-          ],
-          activeIndex: 0,
-          chartData: {
-            datasets: [{ }],
-            labels: ['JAN', 'FEB', 'MAR', 'APR', 'MAY', 'JUN', 'JUL', 'AUG', 'SEP', 'OCT', 'NOV', 'DEC'],
-          },
-          extraOptions: chartConfigs.purpleChartOptions,
-          gradientColors: config.colors.primaryGradient,
-          gradientStops: [1, 0.4, 0],
-          categories: []
-        },
-        purpleLineChart: {
-          extraOptions: chartConfigs.purpleChartOptions,
-          chartData: {
-            labels: ['JUL', 'AUG', 'SEP', 'OCT', 'NOV', 'DEC'],
-            datasets: [{
-              label: "Data",
-              fill: true,
-              borderColor: config.colors.primary,
-              borderWidth: 2,
-              borderDash: [],
-              borderDashOffset: 0.0,
-              pointBackgroundColor: config.colors.primary,
-              pointBorderColor: 'rgba(255,255,255,0)',
-              pointHoverBackgroundColor: config.colors.primary,
-              pointBorderWidth: 20,
-              pointHoverRadius: 4,
-              pointHoverBorderWidth: 15,
-              pointRadius: 4,
-              data: [80, 100, 70, 80, 120, 80],
-            }]
-          },
-          gradientColors: config.colors.primaryGradient,
-          gradientStops: [1, 0.2, 0],
-        },
-        greenLineChart: {
-          extraOptions: chartConfigs.greenChartOptions,
-          chartData: {
-            labels: ['JUL', 'AUG', 'SEP', 'OCT', 'NOV'],
-            datasets: [{
-              label: "My First dataset",
-              fill: true,
-              borderColor: config.colors.danger,
-              borderWidth: 2,
-              borderDash: [],
-              borderDashOffset: 0.0,
-              pointBackgroundColor: config.colors.danger,
-              pointBorderColor: 'rgba(255,255,255,0)',
-              pointHoverBackgroundColor: config.colors.danger,
-              pointBorderWidth: 20,
-              pointHoverRadius: 4,
-              pointHoverBorderWidth: 15,
-              pointRadius: 4,
-              data: [90, 27, 60, 12, 80],
-            }]
-          },
-          gradientColors: ['rgba(66,134,121,0.15)', 'rgba(66,134,121,0.0)', 'rgba(66,134,121,0)'],
-          gradientStops: [1, 0.4, 0],
-        },
-        blueBarChart: {
-          extraOptions: chartConfigs.barChartOptions,
-          chartData: {
-            labels: ['USA', 'GER', 'AUS', 'UK', 'RO', 'BR'],
-            datasets: [{
-              label: "Countries",
-              fill: true,
-              borderColor: config.colors.info,
-              borderWidth: 2,
-              borderDash: [],
-              borderDashOffset: 0.0,
-              data: [53, 20, 10, 80, 100, 45],
-            }]
-          },
-          gradientColors: config.colors.primaryGradient,
-          gradientStops: [1, 0.4, 0],
-        }
-      }
+                      bigLineChart: {
+                        allData: [
+                          [100, 70, 90, 70, 85, 60, 75, 60, 90, 80, 110, 100],
+                          [80, 120, 105, 110, 95, 105, 90, 100, 80, 95, 70, 120],
+                          [60, 80, 65, 130, 80, 105, 90, 130, 70, 115, 60, 130]
+                        ],
+                        activeIndex: 0,
+                        chartData: {
+                          datasets: [{ }],
+                          labels: ['JAN', 'FEB', 'MAR', 'APR', 'MAY', 'JUN', 'JUL', 'AUG', 'SEP', 'OCT', 'NOV', 'DEC'],
+                        },
+                        extraOptions: chartConfigs.purpleChartOptions,
+                        gradientColors: config.colors.primaryGradient,
+                        gradientStops: [1, 0.4, 0],
+                        categories: []
+                      },
+                      purpleLineChart: {
+                        extraOptions: chartConfigs.purpleChartOptions,
+                        chartData: {
+                          labels: ['JUL', 'AUG', 'SEP', 'OCT', 'NOV', 'DEC'],
+                          datasets: [{
+                            label: "Data",
+                            fill: true,
+                            borderColor: config.colors.primary,
+                            borderWidth: 2,
+                            borderDash: [],
+                            borderDashOffset: 0.0,
+                            pointBackgroundColor: config.colors.primary,
+                            pointBorderColor: 'rgba(255,255,255,0)',
+                            pointHoverBackgroundColor: config.colors.primary,
+                            pointBorderWidth: 20,
+                            pointHoverRadius: 4,
+                            pointHoverBorderWidth: 15,
+                            pointRadius: 4,
+                            data: [80, 100, 70, 80, 120, 80],
+                          }]
+                        },
+                        gradientColors: config.colors.primaryGradient,
+                        gradientStops: [1, 0.2, 0],
+                      },
+                      greenLineChart: {
+                        extraOptions: chartConfigs.greenChartOptions,
+                        chartData: {
+                          labels: ['JUL', 'AUG', 'SEP', 'OCT', 'NOV'],
+                          datasets: [{
+                            label: "My First dataset",
+                            fill: true,
+                            borderColor: config.colors.danger,
+                            borderWidth: 2,
+                            borderDash: [],
+                            borderDashOffset: 0.0,
+                            pointBackgroundColor: config.colors.danger,
+                            pointBorderColor: 'rgba(255,255,255,0)',
+                            pointHoverBackgroundColor: config.colors.danger,
+                            pointBorderWidth: 20,
+                            pointHoverRadius: 4,
+                            pointHoverBorderWidth: 15,
+                            pointRadius: 4,
+                            data: [90, 27, 60, 12, 80],
+                          }]
+                        },
+                        gradientColors: ['rgba(66,134,121,0.15)', 'rgba(66,134,121,0.0)', 'rgba(66,134,121,0)'],
+                        gradientStops: [1, 0.4, 0],
+                      },
+                      blueBarChart: {
+                        extraOptions: chartConfigs.barChartOptions,
+                        chartData: {
+                          labels: ['USA', 'GER', 'AUS', 'UK', 'RO', 'BR'],
+                          datasets: [{
+                            label: "Countries",
+                            fill: true,
+                            borderColor: config.colors.info,
+                            borderWidth: 2,
+                            borderDash: [],
+                            borderDashOffset: 0.0,
+                            data: [53, 20, 10, 80, 100, 45],
+                          }]
+                        },
+                        gradientColors: config.colors.primaryGradient,
+                        gradientStops: [1, 0.4, 0],
+                      }
+                    }
     },
     computed: {
       enableRTL() {
@@ -275,6 +283,16 @@
         this.i18n.locale = 'en';
         this.$rtl.disableRTL();
       }
+    },
+    get() {
+      fetch('http://ecocim-backend-brisatelitv2.beit.co.id/api/Dashboard/GetDashboardWidgetData?widget=PSCF_UPS_RoomTH&chart_name=PSCF_UPS_RoomTH')
+          .then(function(response) {
+            return response.json()
+          })
+          .then(function(data) {
+              const data2 = JSON.stringify(data)
+              console.log(data2);
+          })
     }
   };
 </script>
